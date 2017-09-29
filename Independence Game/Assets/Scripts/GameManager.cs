35cb588063ee11a47d4086f55cc5d18a78269f4b
broxigar91 <ichigobiar@gmail.com>
2017-09-30 00:53:01 +0200

    GameManager added

---
+++ Independence Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameState
+    {
+        PAUSE,
+        ACTIVE
+    }
+
+    public static GameManager instance=null;
+    private GameState estadoJuego;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        estadoJuego = GameState.ACTIVE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
